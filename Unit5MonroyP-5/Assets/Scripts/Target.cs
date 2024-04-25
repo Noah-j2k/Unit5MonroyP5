4ac2db55b82268378e3759d218b7c23adc365f9d
christopher lovato <noahmonroy17@gmail.com>
2024-04-25 15:28:47 -0700

    Step 8 or 7
    finish 3

---
+++ Unit5MonroyP-5/Assets/Scripts/Target.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTourqe = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(Vector3.up * Random.Range(minSpeed, maxSpeed), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-maxTourqe,maxTourqe), Random.Range(maxTourqe,maxTourqe), Random.Range(maxTourqe,maxTourqe), ForceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnnTriggerEnter(Collider other)
+    {
+        Destroy (gameObject);
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);  
+    }
+    float RandomTorqe()
+    {
+        return Random.Range(-maxTourqe, maxTourqe);
+    }
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+}
